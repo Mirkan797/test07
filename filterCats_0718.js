cb84132963528d95aaf6955b849a9ccd4ecd03fa
Mirkan797 <ghkd79797@naver.com>
2022-07-18 09:44:41 +0900

    필터캣츠0718

---
+++ filterCats_0718.js
@@ -0,0 +1,54 @@
+const data = [
+{
+    name: '나나',
+    colors: ['yellow', 'white'],
+    age: 7,
+    ear: 'unfolded'
+},
+{
+    name: '차이',
+    colors: ['white', 'yellow', 'brown'],
+    age: 3,
+    ear: 'unfolded'
+},
+{
+    name: '모나',
+    colors: ['black', 'white'],
+    age: 2,
+    ear: 'unfolded'
+},
+{
+    name: '레이',
+    colors: ['gray', 'white'],
+    age: 7,
+    ear: 'unfolded'
+},
+{
+    name: '가상의 고양이',
+    colors: ['gray', 'black'],
+    age: 10,
+    ear: 'folded'
+}, 
+null,
+]
+
+// 털 색이 까만색이 포함되어 있으면서
+// 귀가 접혀있지 않은 고양이들을 뽑기
+function filterCats(cats) {
+    let results = []
+
+    for(let i = 0; i < cats.length; i++) {
+        const cat = cats[i]
+        if (cat &&
+            cat.colors.includes('black') &&
+            cat.ear === 'unfolded') {
+                results.push(cat.name)
+            }
+    }
+
+    return results
+}
+ 
+const filteredCatsName = filterCats(data)
+console.log(filteredCatsName)
+document.querySelector('body').innerHTML = filterCatsName
