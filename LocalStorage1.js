e462c58379746f2193eefbbfca6f8302f38afd41
Mirkan797 <ghkd79797@naver.com>
2022-07-19 09:21:12 +0900

    Local Storage Test1_0719

--- LocalStorage1.js
+++ LocalStorage1.js
@@ -11,3 +11,17 @@ document.querySelector('#save').addEventListener('click', () => {
 document.querySelector('#remove').addEventListener('click', () => {
     localStorage.removeItem('name')
 })
+
+//    <html>
+//        <head>
+//            <title> KDT Storage Test</title>
+//        </head>
+//        <body>
+//            <main class="app">
+//                <input type="text" id="name">
+//                    <button id="save">Save</button>
+//                    <button id="remove">Remove</button>
+//           </main>
+//            <script src="./src/storageTest.js"></script>
+//        </body>
+//    </html>
