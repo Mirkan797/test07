dcf603703690c5a91d05e313c6762f2338ff4e71
Mirkan797 <ghkd79797@naver.com>
2022-07-22 09:37:42 +0900

    api.js0722

--- src/api.js
+++ src/api.js
@@ -1,16 +1,18 @@
-export function request (url, successCallback, failCallback) {
-    const xhr = new XHLHttpRequest()
-    xhr.addEventListener("load", (e) => {
-        if (xhr.readyState === 4) {
-            if (xhr.status == 200) {
-                uccessCallback(JSON.parse(xhr.reponseText))
-            } else {
-                failCallback
+export function request (url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XHLHttpRequest()
+        xhr.addEventListener("load", (e) => {
+            if (xhr.readyState === 4) {
+                if (xhr.status == 200) {
+                    resolve(JSON.parse(xhr.reponseText))
+                } else {
+                    reject(xhr.statusText)
+                }
             }
-        }
-    })
-    xhr.addEventListener('error', (e) => failCallback(xhr.statusText))
+        })
+        xhr.addEventListener('error', (e) => reject(xhr.statusText))
 
-    xhr.open('GET', url)
-    xhr.send()
+        xhr.open('GET', url)
+        xhr.send()
+    })
 }
